f2dd50d49efdb5ba9c8d46a929962ce1f4b1db4e
VanshDeshwal <vanshdeshwal66@gmail.com>
2020-10-12 13:43:33 -0400

    new

--- shells.sh
+++ shells.sh
@@ -4,6 +4,41 @@
 
 
 
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPTNAME="$0"
+ARGS="$@"
+BRANCH="main"
+
+self_update() {
+    cd $SCRIPTPATH
+    git fetch
+
+    [ -n $(git diff --name-only origin/$BRANCH | grep $SCRIPTNAME) ] && {
+        echo "Found a new version of me, updating myself..."
+        git pull --force
+        git checkout $BRANCH
+        git pull --force
+        echo "Running the new version..."
+        exec "$SCRIPTNAME" "$@"
+
+        # Now exit this old instance
+        exit 1
+    }
+    echo "Already the latest version."
+}
+
+main() {
+   echo "Running"
+}
+
+self_update
+main
+
+
+
+
 error=$(cat /sys/class/net/tun0/operstate 2>/dev/null)
 e=0
 interface=0
